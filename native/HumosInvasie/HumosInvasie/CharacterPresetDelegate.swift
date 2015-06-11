e6886a9e038b2adfdda1b9222afefcd6733f4fe5
MrSpijker <wannes.monchy@student.howest.be>
2015-06-11 11:08:56 +0200

    longpress integrated

---
+++ native/HumosInvasie/HumosInvasie/CharacterPresetDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  MasterDelegate.swift
+//  SlaveForYou
+//
+//  Created by MrSpijker on 05/03/15.
+//  Copyright (c) 2015 MrSpijker. All rights reserved.
+//
+
+protocol AvatarPresetDelegate:class{
+    func didLongpressPreset(thePreset:AnyObject)
+    
+}
